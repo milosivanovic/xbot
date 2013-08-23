6fb413bcdb31612ecee2a73637b1372263fa0eb9
Milos Ivanovic <milosivanovic@users.noreply.github.com>
2013-08-24 01:57:44 +1300

    Fix newlines for botdb.py

--- xbot/modules/botdb.py
+++ xbot/modules/botdb.py
@@ -7,7 +7,7 @@ class BotDB(object):
 		self.user = bot.config.get('module: botdb', 'db_user')
 		self.pw = bot.config.get('module: botdb', 'db_pass')
 		self.db = bot.config.get('module: botdb', 'db_name')
-	
+
 	def connect(self):
 		if self.host and self.user and self.pw and self.db:
 			return MySQLdb.connect(host=self.host, user=self.user, passwd=self.pw, db=self.db)
