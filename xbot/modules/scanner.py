da5556b03970c0bab1c9b96d82e6df4ab5bebcf6
Milos Ivanovic <milosivanovic@users.noreply.github.com>
2015-11-02 01:33:57 +1300

    Fix YouTube title extraction bug, disable useless bot feature and remove newlines

--- xbot/modules/scanner.py
+++ xbot/modules/scanner.py
@@ -5,33 +5,34 @@ import cleverbot
 
 def scan(bot, message = None):
 	results = []
-	
+
 	if message:
 		bot.remote['message'] = message
-		
+
 	message_lowercase = bot.remote['message'].lower()
-	
+
 	# scan for youtube links and show title
 	for code in re.findall('(?:youtube\.com\/watch\?|youtu\.be/)(?:[A-Za-z0-9-_\.&%#=]*v=)?([A-Za-z0-9-_]+)', bot.remote['message']):
 		results.append(youtube_title(code))
-	
+
 	# someone is talking to the bot
 	if re.search('^%s(?:\:|,)' % re.escape(bot.nick.lower()), message_lowercase):
+		#bot._sendq(("NOTICE", bot.remote['nick']), "This feature has been disabled.")
+
 		if 'cleverbot' not in bot.inv: bot.inv['cleverbot'] = {}
 		if bot.remote['receiver'] not in bot.inv['cleverbot']:
 			bot.inv['cleverbot'][bot.remote['receiver']] = cleverbot.CleverBot()
 		query = bot.remote['message'][len(bot.nick)+2:].decode('ascii', 'ignore')
 		results.append("%s: %s" % (bot.remote['nick'], re.compile('cleverbot', re.IGNORECASE).sub(bot.nick, bot.inv['cleverbot'][bot.remote['receiver']].query(query))))
-		#bot._sendq(("NOTICE", bot.remote['nick']), "This feature has been disabled.")
-	
+
 	# per 10% chance, count uppercase and act shocked
-	if len(bot.remote['message']) > 2 and random.random() > 0.9:
-		if count_upper(bot.remote['message']) > 80:
-			time.sleep(4)
-			results.append(random.choice([':' + 'O' * random.randint(1, 10), 'O' * random.randint(1, 10) + ':']))
-	
-	# per 1% chance, butt into someone's conversation
-	if random.random() > 0.99:
+	#if len(bot.remote['message']) > 2 and random.random() > 0.9:
+	#	if count_upper(bot.remote['message']) > 80:
+	#		time.sleep(4)
+	#		results.append(random.choice([':' + 'O' * random.randint(1, 10), 'O' * random.randint(1, 10) + ':']))
+
+	# per 0.01% chance, butt into someone's conversation
+	'''if random.random() > 0.999:
 		if not bot.remote['message'].startswith("\x01"):
 			words = bot.remote['message'].split()
 			if len(words) > 2:
@@ -43,9 +44,12 @@ def scan(bot, message = None):
 							if len(word) > 4 and m > 0:
 								if random.random() > 0.3:
 									words[m] = words[m][:-4] + "butt"
-			
-				results.append(' '.join(words))
-	
+
+				results.append(' '.join(words))'''
+
+	if 'gface' in bot.remote['message']:
+		results.append('\x28\x20\xE2\x89\x96\xE2\x80\xBF\xE2\x89\x96\x29')
+
 	results = [result for result in results if result is not None]
 	try: return '\n'.join(results)
 	except TypeError: return None
@@ -63,16 +67,20 @@ def youtube_title(code):
 		# embed request not allowed? fallback to HTML (slower)
 		if error.code == 401:
 			import lxml.html
-			title = lxml.html.document_fromstring(urllib2.urlopen('http://www.youtube.com/watch?v=%s' % code, timeout = 5).read().decode('utf-8')).xpath("//title/text()")[0].split("\n")[1].strip()
+			try:
+				title = lxml.html.document_fromstring(urllib2.urlopen('http://www.youtube.com/watch?v=%s' % code, timeout = 5).read().decode('utf-8')).xpath("//title/text()")[0].replace(' - YouTube', '')
+				#[0].split("\n")[1].strip()
+			except IndexError:
+				title = None
 		else:
 			title = None
 			if error.code != 404:
 				raise
-	
+
 	if title:
 		if title != "YouTube - Broadcast Yourself.":
 			return "YouTube: \x02%s\x02" % title.encode('utf-8')
-			
+
 	return None
 
 def count_upper(str):
