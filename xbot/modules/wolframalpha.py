f06e0614237b71b4203d2eec27979a27ebb91a8d
Milos Ivanovic <milosivanovic@users.noreply.github.com>
2015-11-03 01:36:32 +1300

    Add more Wolfram|Alpha supported results, and refactor some code

--- xbot/modules/wolframalpha.py
+++ xbot/modules/wolframalpha.py
@@ -3,7 +3,7 @@ import lxml.etree
 
 def wa(bot, args):
 	if len(args) > 1:
-		response = urllib2.urlopen("http://api.wolframalpha.com/v2/query?appid=%s&input=%s&format=plaintext" % (bot.config.get('module: wolframalpha', 'wa_app_id'), urllib2.quote(' '.join(args[1:]))), timeout = 10)
+		response = urllib2.urlopen("http://api.wolframalpha.com/v2/query?appid=%s&input=%s&format=plaintext&ip=103.250.91.91" % (bot.config.get('module: wolframalpha', 'wa_app_id'), urllib2.quote(' '.join(args[1:]))), timeout = 20)
 		result = lxml.etree.parse(response)
 		acceptable = [
 			'Result', 'Results', 'Solution', 'Value', 'Name', 'Derivative', 'Indefinite integral', 'Distance', 'Current result*',
@@ -13,15 +13,16 @@ def wa(bot, args):
 			'Address properties', 'Web hosting information', 'Current age', 'Basic information', 'Latest result', 'Response',
 			'Names and airport codes', 'Latest recorded weather *', 'Series information', 'Latest trade', 'Definitions of *',
 			'Possible interpretation*', 'Lifespan', 'Cipher text', 'Statement', 'Events on *', 'Time span', 'Unicode block',
-			'Eclipse date', 'Total eclipse*', 'Solar wind', 'Weather forecast for *', 'Notable events in *', 'Events on *',
-			'Possible sequence identification', 'Length of data', 'Properties', 'Approximate results', 'Summary', 'Nearest named HTML colors'
+			'Eclipse date', 'Total eclipse*', 'Solar wind', 'Weather forecast for *', 'Notable events in *', 'Events on *', 'Basic elemental properties',
+			'Possible sequence identification', 'Length of data', 'Properties', 'Approximate results', 'Summary', 'Nearest named HTML colors',
+			'Alternate form', 'Estimated result', 'Estimated results'
 		]
 		for title in acceptable:
 			success = xml(result, title)
 			if success: break
 		failure = result.xpath("/queryresult[@success='false']")
 		if success:
-			success = unicode(success.replace("\:", "\u"))
+			success = success.replace("\:", "\u")
 			return success.encode('utf-8').replace("Wolfram|Alpha", bot.name).replace("Stephen Wolfram", "Milos Ivanovic").strip()
 		elif failure:
 			alternatives = result.xpath("/queryresult/relatedexamples/relatedexample[@input]")
@@ -36,6 +37,10 @@ def wa(bot, args):
 
 def xml(result, title):
 	if '*' in title:
-		return '\n'.join(result.xpath("/queryresult[@success='true']/pod[contains(@title, '%s')]/subpod/plaintext/text()" % title.replace("*", "")))
+		result = '\n'.join(result.xpath("/queryresult[@success='true']/pod[contains(@title, '%s')]/subpod/plaintext/text()" % title.replace("*", "")))
 	else:
-		return '\n'.join(result.xpath("/queryresult[@success='true']/pod[@title='%s']/subpod/plaintext/text()" % title))
+		result = '\n'.join(result.xpath("/queryresult[@success='true']/pod[@title='%s']/subpod/plaintext/text()" % title))
+	if len(result) > 445:
+		return "!calc: possible flood. Result cancelled."
+	else:
+		return result
