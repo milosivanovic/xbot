8029e5e253d1aa5b25a2a693bb4b13d5f2934c0c
Milos Ivanovic <milosivanovic@users.noreply.github.com>
2015-11-04 01:42:50 +1300

    Add support for Urban Dictionary

---
+++ xbot/modules/ud.py
@@ -0,0 +1,39 @@
+import urllib, urllib2
+import json, re
+
+def unicode_truncate(s, length, encoding='utf-8'):
+	encoded = s.encode(encoding)[:length]
+	return encoded.decode(encoding, 'ignore')
+
+def ud(bot, args):
+	if len(args) > 1:
+		support = ['en']
+		if args[1].startswith('@'):
+			lang = args[1][1:]
+			if lang in support:
+				query = ' '.join(args[2:])
+			else:
+				return "!%s: unsupported language (use: %s)" % (args[0], ','.join(support))
+		else:
+			lang = 'en'
+			query = ' '.join(args[1:])
+
+		result = None
+		friendly_url = "http://www.urbandictionary.com/define.php?term=%s" % urllib.quote_plus(query)
+
+		if lang == 'en':
+			url = 'http://api.urbandictionary.com/v0/define?term=%s' % urllib.quote_plus(query)
+			json_text = urllib2.urlopen(url).read()
+			json_obj = json.loads(json_text)
+			try:
+				result = json_obj['list'][0]['definition']
+			except IndexError:
+				pass
+		if result:
+			text = ''.join(result)
+			overhead = len(friendly_url)
+			if len(text) > (435-overhead):
+				text = "%s... (%s)" % (unicode_truncate(text, 435-overhead), friendly_url)
+			return re.sub('[\r\n]+', ' | ', re.sub(' +', ' ', text.strip())).encode('utf-8')
+		return '!%s: no urban dictionary result found in "%s"' % (args[0], lang)
+	return "Usage: !%s [@en] <word>" % args[0]
